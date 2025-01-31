f7734b14d17833ee7de78bc25886fffe45cfeb41
HOSHIKOSPC\Cliente <giovani.mamprin@icloud.com>
2025-01-31 18:37:00 -0500

    Started Assignment 1 - added globe sprite.

---
+++ Assets/Scripts/SpinningGlobe.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningGlobe : MonoBehaviour
+{
+    //Variables:
+
+    //Globe Quadrant
+    int quad;
+
+    //Rotation
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
