e21d31c745395a51d0131fc5f670cceddd7a62eb
HOSHIKOSPC\Cliente <giovani.mamprin@icloud.com>
2025-01-31 19:05:56 -0500

    Added globe that spins when pressing an arrow key.

--- Assets/Scripts/SpinningGlobe.cs
+++ Assets/Scripts/SpinningGlobe.cs
@@ -7,9 +7,15 @@ public class SpinningGlobe : MonoBehaviour
     //Variables:
 
     //Globe Quadrant
-    int quad;
+    public int quad = 1;
 
-    //Rotation
+    [Range(0,360)]
+    public int targetAngle;
+
+    bool currentlySpinning;
+
+    //Rotation Speed
+    public float rotationSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,19 @@ public class SpinningGlobe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Rotation
+        Vector3 spin = transform.eulerAngles;
+
+        //Change current quadrant based on arrow input
+        if (Input.GetKeyDown(KeyCode.RightArrow) && currentlySpinning == false)
+        {
+            targetAngle += 90;
+        }
+
+        if (spin.z < targetAngle)
+        {
+            spin.z += rotationSpeed;
+        }
+        transform.eulerAngles = spin;
     }
 }
