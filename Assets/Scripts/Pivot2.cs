1fa45d89d4c538e17e77cc2a5a6502d701a7dd49
HOSHIKOSPC\Cliente <giovani.mamprin@icloud.com>
2025-02-01 16:27:49 -0500

    Added a second rotation pivot with offset for the second set of arms and legs

--- Assets/Scripts/Pivot2.cs
+++ Assets/Scripts/Pivot2.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpinningGlobe : MonoBehaviour
+public class Pivot2 : MonoBehaviour
 {
     //Variables:
 
@@ -10,17 +10,19 @@ public class SpinningGlobe : MonoBehaviour
     public int quad = 0;
 
     //Flag for the globe's spinning state
-    bool currentlySpinning = false;
+    public bool currentlySpinning = false;
 
     //Rotation Speed
     public float rotationSpeed;
     //Rotation Amount
     public float rotationAmount;
 
+    public float rot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -28,32 +30,47 @@ public class SpinningGlobe : MonoBehaviour
     {
         //Rotation
         Vector3 spin = transform.eulerAngles;
+        rot = spin.z;
 
         //Change current quadrant when the space bar is pressed
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKeyDown(KeyCode.Space) && quad == 0 || Input.GetKeyDown(KeyCode.Space) && quad >= 2)
         {
             currentlySpinning = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             quad += 1;
         }
 
         //Rotate globe until it reaches the next quadrant
-        if (currentlySpinning == true && rotationAmount <= 90)
+        if (currentlySpinning == true && rotationAmount <= 90 )
         {
-            spin.z -= rotationSpeed ;
+            spin.z -= rotationSpeed;
             rotationAmount += rotationSpeed;
-        } else //Set rotation to quad * 90 so there is no gradual offset when spinning multiple times
+        }
+        else if (quad == 1) //Set rotation to quad * 90 so there is no gradual offset when spinning multiple times
         {
             currentlySpinning = false;
             rotationAmount = 0;
-            spin.z = 360 - (quad * 90);
+            spin.z = 270;
+        } else
+        {
+            currentlySpinning = false;
+            rotationAmount = 0;
         }
-       
+
+
         //Reset Quadrant variable
         if (quad > 3)
         {
+            spin.z = 90;
             quad = 0;
         }
 
         transform.eulerAngles = spin;
     }
 }
+
+
